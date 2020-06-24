95b6bd9ed3f897820e7215669a0553352f8323ad
Bryan Braun <bbraun7@gmail.com>
2020-06-24 00:36:53 -0400

    docs: add a desmos.com formula to the wave code comments, for reference

--- docs/demos/wave/demo.js
+++ docs/demos/wave/demo.js
@@ -24,6 +24,7 @@ function simpleWave(x, y, t) {
   let frequencyConstant = 3;
   let scaledTime = t * 3;
 
+  // For reference, see https://www.desmos.com/calculator/bp9t79pfa0
   return y < 5 * Math.sin((x - scaledTime) / frequencyConstant);
 }
 
